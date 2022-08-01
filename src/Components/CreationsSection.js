d7618c8d3fcb53ab8a05c7ddbdf76dc2cc472383
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-08-01 16:38:07 -0300

    Made it responsive. Desktop, mobile and mid desigs done.

--- src/Components/CreationsSection.js
+++ src/Components/CreationsSection.js
@@ -17,22 +17,42 @@ import pocketBorealis from "../images/desktop/image-pocket-borealis.jpg";
 import curiosity from "../images/desktop/image-curiosity.jpg";
 import fisheye from "../images/desktop/image-fisheye.jpg";
 
+import deepEarthMobile from "../images/mobile/image-deep-earth.jpg";
+import nightArcadeMobile from "../images/mobile/image-night-arcade.jpg";
+import soccerTeamMobile from "../images/mobile/image-soccer-team.jpg";
+import carGridMobile from "../images/mobile/image-grid.jpg";
+import fromAboveMobile from "../images/mobile/image-from-above.jpg";
+import pocketBorealisMobile from "../images/mobile/image-pocket-borealis.jpg";
+import curiosityMobile from "../images/mobile/image-curiosity.jpg";
+import fisheyeMobile from "../images/mobile/image-fisheye.jpg";
+
 function CreationsSection(){
+
+    function itsMobile() {
+        if (window.matchMedia("(max-width: 960px)").matches) {
+            return true;
+        }
+        return false;
+    }
+
     return(
         <div className='creations-container'>
             <div className='creations-header-container'>
                 <h2 className='creations-title'>OUR CREATIONS</h2>
-                <button className='button'>SEE ALL</button>
+                <button className={itsMobile() ? 'button hidden' : 'button' }>SEE ALL</button>
             </div>
             <div className='grid-container'>
-                <CreationCard imgSrc={deepEarth} fstLvlTitle="DEEP" sndLvlTitle="EARTH" />
-                <CreationCard imgSrc={nightArcade} fstLvlTitle="NIGHT" sndLvlTitle="ARCADE" />
-                <CreationCard imgSrc={soccerTeam} fstLvlTitle="SOCCER" sndLvlTitle="TEAM VR" />
-                <CreationCard imgSrc={carGrid} fstLvlTitle="THE" sndLvlTitle="GRID" />
-                <CreationCard imgSrc={fromAbove} fstLvlTitle="FROM UP" sndLvlTitle="ABOVE VR" />
-                <CreationCard imgSrc={pocketBorealis} fstLvlTitle="POCKET" sndLvlTitle="BOREALIS" />
-                <CreationCard imgSrc={curiosity} fstLvlTitle="THE" sndLvlTitle="CURIOSITY" />
-                <CreationCard imgSrc={fisheye} fstLvlTitle="MAKE IT" sndLvlTitle="FISHEYE" />
+                <CreationCard imgSrc={itsMobile() ? deepEarthMobile : deepEarth} fstLvlTitle="DEEP" sndLvlTitle="EARTH" />
+                <CreationCard imgSrc={itsMobile() ? nightArcadeMobile : nightArcade} fstLvlTitle="NIGHT" sndLvlTitle="ARCADE" />
+                <CreationCard imgSrc={itsMobile() ? soccerTeamMobile : soccerTeam} fstLvlTitle="SOCCER" sndLvlTitle="TEAM VR" />
+                <CreationCard imgSrc={itsMobile() ? carGridMobile : carGrid} fstLvlTitle="THE" sndLvlTitle="GRID" />
+                <CreationCard imgSrc={itsMobile() ? fromAboveMobile : fromAbove} fstLvlTitle="FROM UP" sndLvlTitle="ABOVE VR" />
+                <CreationCard imgSrc={itsMobile() ? pocketBorealisMobile : pocketBorealis} fstLvlTitle="POCKET" sndLvlTitle="BOREALIS" />
+                <CreationCard imgSrc={itsMobile() ? curiosityMobile : curiosity} fstLvlTitle="THE" sndLvlTitle="CURIOSITY" />
+                <CreationCard imgSrc={itsMobile() ? fisheyeMobile : fisheye} fstLvlTitle="MAKE IT" sndLvlTitle="FISHEYE" />
+            </div>
+            <div className='creations-header-container'>
+                <button className={!itsMobile() ? 'button hidden' : 'button' }>SEE ALL</button>
             </div>
         </div>
     );
